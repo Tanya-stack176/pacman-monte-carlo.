a18098f40fb7fc656cc40054793e7ac5401bc442
Tanya-stack176 <skalatskaja3@gmail.com>
2024-08-24 22:37:40 +0300

    Update monte_carlo_pacman.py

--- monte_carlo_pacman.py
+++ monte_carlo_pacman.py
@@ -36,7 +36,8 @@ class MonteCarloPacman(Pacman):
 
 # Використання класу в грі:
 if __name__ == "__main__":
-    maze = Maze("maze.txt")
+    maze = Maze("maze2.txt")
     pacman = MonteCarloPacman()
     ghosts = [Ghost("ghost1", maze), Ghost("ghost2", maze)]
-    pacman.play_game(maze, ghosts)
+    pacman.play_game(maze, print("List length:", len(my_list))
+print("Accessing index:", index)
